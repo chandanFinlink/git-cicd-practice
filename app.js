6c5c2ef4f8e25b10602fdd81b05d1e350b53710a
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-06-26 20:06:00 +0530

    Fix: restore correct string reversal logic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // A simple function to reverse a string
 function reverseString(str) {
-    return str; //.split('').reverse().join('');
+    return str.split('').reverse().join('');
 }
 module.exports = reverseString;
