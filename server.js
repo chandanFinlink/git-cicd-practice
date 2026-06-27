db313843183ac175186a98383a1762831a0fe6d9
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-06-27 18:44:35 +0530

    Revert "Featutre: add broken anylitcs integration tracking module"
    This reverts commit e592bcc851fd43240afdd71dd1f5345609bedd09.

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // Import the server configuration from app.js
 const { server } = require('./app');
-// Deliberately breaking the import to simulate a bad developer mistake
-const { server } = require('./app-BROKEN-FILE');
 
 const PORT = process.env.PORT || 4000;
 
